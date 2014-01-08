e5f5b0f36951f79eb441062bd0b0b35a52dda7c9
Phanx-28751 <Phanx@users.noreply.github.com>
2014-01-08 02:12:46 +0000

    - Add borders to merchant repair and buyback buttons - Remove ugly backdrops from profession button names - Add quality coloring to quest progress and reward items - Improve borders in spellbook "core abilities" tab - Add borders to the glyph UI - Add borders to pet battle action buttons - Hide empty slots in the mount journal - Add borders to the pet journal, with quality coloring - Add borders to the talent UI

--- Blizzard.lua
+++ Blizzard.lua
@@ -6,16 +6,11 @@
 	See the accompanying README and LICENSE files for more information.
 ----------------------------------------------------------------------]]
 
+local _, PhanxBorder = ...
+local _, _, _, Masque = GetAddOnInfo("Masque")
 local AddBorder = PhanxBorder.AddBorder
 local AddShadow = PhanxBorder.AddShadow
-local Masque = select(4, GetAddOnInfo("Masque"))
-
-local COLOR_BY_CLASS = true
-
-------------------------------------------------------------------------
---	Addon frames
-------------------------------------------------------------------------
-
+local config = PhanxBorder.config
 local noop = function() end
 
 local applyFuncs = { }
@@ -76,14 +71,11 @@ tinsert(applyFuncs, function()
 		local f = _G[borderedTooltips[i]]
 		if f then
 			--print("Adding border to", borderedTooltips[i])
-
 			for _, region in pairs(borderedTooltipRegions) do
 				f[region]:Hide()
 			end
-
 			f:SetBackdrop(GameTooltip:GetBackdrop())
 			AddBorder(f)
-
 			tremove(borderedTooltips, i)
 		end
 	end
@@ -95,7 +87,7 @@ tinsert(applyFuncs, function()
 end)
 
 ------------------------------------------------------------------------
---	Miscellaneous frames
+--	FrameXML
 ------------------------------------------------------------------------
 
 tinsert(applyFuncs, function()
@@ -128,6 +120,10 @@ tinsert(applyFuncs, function()
 		["WorldMapCompareTooltip3"] = false,
 		["WorldMapTooltip"] = false,
 
+		["MerchantRepairItemButton"] = 3,
+		["MerchantRepairAllButton"] = 3,
+		["MerchantBuyBackItemItemButton"] = 5,
+
 		["PrimaryProfession1SpellButtonBottom"] = 3,
 		["PrimaryProfession1SpellButtonTop"] = 3,
 		["PrimaryProfession2SpellButtonBottom"] = 3,
@@ -143,6 +139,9 @@ tinsert(applyFuncs, function()
 	}) do
 		-- print("Adding border to " .. frame)
 		AddBorder(_G[frame], nil, offset)
+		if strmatch(frame, "aryProfession") then
+			_G[frame.."NameFrame"]:SetTexture("")
+		end
 	end
 
 	GhostFrame:SetWidth(140)
@@ -153,7 +152,10 @@ tinsert(applyFuncs, function()
 	GhostFrameMiddle:SetTexture("")
 	GhostFrameContentsFrameIcon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
 
+	---------------------------------------------------------------------
 	--	Character frame
+	---------------------------------------------------------------------
+
 	for _, slot in pairs({
 		"CharacterHeadSlot",
 		"CharacterNeckSlot",
@@ -218,40 +220,71 @@ tinsert(applyFuncs, function()
 	end)
 
 
+	---------------------------------------------------------------------
 	-- Mailbox
-	for i = 1, 12 do
-		AddBorder(_G["OpenMailAttachmentButton"..i])
-		AddBorder(_G["SendMailAttachment"..i])
-	end
+	---------------------------------------------------------------------
+
 	for i = 1, 7 do
 		AddBorder(_G["MailItem"..i.."Button"])
 	end
 
+	for i = 1, 12 do
+		AddBorder(_G["OpenMailAttachmentButton"..i])
+		AddBorder(_G["SendMailAttachment"..i])
+	end
+
+	---------------------------------------------------------------------
 	-- Merchant frame
+	---------------------------------------------------------------------
+
+	MerchantBuyBackItemNameFrame:SetTexture("")
+
 	for i = 1, 12 do
 		AddBorder(_G["MerchantItem"..i.."ItemButton"])
 	end
 
+	---------------------------------------------------------------------
 	-- Pet stable
+	---------------------------------------------------------------------
+
 	for i = 1, 10 do
-		AddBorder(_G["PetStableStabledPet"])
+		AddBorder(_G["PetStableStabledPet"..i])
 	end
 
-	-- Quests
-	AddBorder(QuestInfoSkillPointFrame)
+	---------------------------------------------------------------------
+	-- Quest frames
+	---------------------------------------------------------------------
+
+	local function AddItemBorder(f)
+		AddBorder(f)
+		local icon = f.icon or f.Icon or _G[f:GetName().."IconTexture"]
+		local name = f.name or f.Name or _G[f:GetName().."Name"]
+		if icon then
+			icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+		end
+		if name then
+			name:SetFontObject("QuestFontNormalSmall")
+			_G[f:GetName().."NameFrame"]:SetTexture("")
+		end
+	end
+
+	AddItemBorder(QuestInfoRewardSpell)
+	QuestInfoRewardSpellSpellBorder:SetTexture("")
+
+	AddItemBorder(QuestInfoSkillPointFrame)
+
 	for i = 1, MAX_NUM_ITEMS do
-		local f = _G["QuestInfoItem"..i]
-		f.name:SetFontObject("QuestFontNormalSmall")
-		_G["QuestInfoItem"..i.."NameFrame"]:SetTexture("")
-
-		local iconFrame = CreateFrame("Frame", nil, f)
-		iconFrame:SetAllPoints(f.icon)
-		AddBorder(iconFrame, nil, 3)
-		f.iconFrame = iconFrame
+		AddItemBorder(_G["QuestInfoItem"..i])
 	end
+
 	hooksecurefunc("QuestInfo_Display", function()
+		QuestInfoRewardSpell:SetBorderSize(nil, -3, -102, 4, -8)
+
+		QuestInfoSkillPointFrame:SetBorderSize()
+
 		for i = 1, MAX_NUM_ITEMS do
 			local f = _G["QuestInfoItem"..i]
+			f:SetBorderSize(nil, 3, -104, 3, 3)
 			local colored
 			if f.type then
 				local link = GetQuestItemLink(f.type, i)
@@ -259,48 +292,54 @@ tinsert(applyFuncs, function()
 					local _, _, quality = GetItemInfo(link)
 					if quality and quality > 1 then
 						local color = ITEM_QUALITY_COLORS[quality]
-						f.iconFrame:SetBorderColor(color.r, color.g, color.b)
+						f:SetBorderColor(color.r, color.g, color.b)
 						colored = true
 					end
 				end
 			end
 			if not colored then
-				f.iconFrame:SetBorderColor()
+				f:SetBorderColor()
 			end
 		end
 	end)
-	for i = 1, MAX_REQUIRED_ITEMS do
-		local f = _G["QuestProgressItem"..i]
-		f.name:SetFontObject("QuestFontNormalSmall")
-		_G["QuestProgressItem"..i.."NameFrame"]:SetTexture("")
 
-		local iconFrame = CreateFrame("Frame", nil, f)
-		iconFrame:SetAllPoints(f.icon)
-		AddBorder(iconFrame, nil, 3)
-		f.iconFrame = iconFrame
+	hooksecurefunc("QuestInfo_ShowRewards", function()
+		print("QuestInfo_ShowRewards")
+	end)
+
+	for i = 1, MAX_REQUIRED_ITEMS do
+		AddItemBorder(_G["QuestProgressItem"..i])
 	end
+
 	hooksecurefunc("QuestFrameProgressItems_Update", function()
 		print("QuestFrameProgressItems_Update") -- #TODO
 		for i = 1, MAX_REQUIRED_ITEMS do
 			local f = _G["QuestProgressItem"..i]
+			f:SetBorderSize(nil, 4, -102, 4, 3)
 			local colored
 			if f:IsShown() then
-
 			end
 			if not colored then
-				f.iconFrame:SetBorderColor()
+				f:SetBorderColor()
 			end
 		end
 	end)
 
+	---------------------------------------------------------------------
 	-- Static popups
+	---------------------------------------------------------------------
+
 	AddBorder(StaticPopup1ItemFrame)
 
+	---------------------------------------------------------------------
 	-- Trade window
+	---------------------------------------------------------------------
+
 	for i = 1, 7 do
 		AddBorder(_G["TradePlayerItem"..i.."ItemButton"])
 		AddBorder(_G["TradeRecipientItem"..i.."ItemButton"])
 	end
+
 	hooksecurefunc("TradeFrame_UpdatePlayerItem", function(id)
 		local link = GetTradePlayerItemLink(id)
 		if link then
@@ -312,6 +351,7 @@ tinsert(applyFuncs, function()
 		end
 		_G["TradePlayerItem"..i.."ItemButton"]:SetBorderColor()
 	end)
+
 	hooksecurefunc("TradeFrame_UpdateTargetItem", function(id)
 		local _, _, _, quality = GetTradeTargetItemInfo(id)
 		if quality and quality > 1 then
@@ -322,12 +362,14 @@ tinsert(applyFuncs, function()
 		end
 	end)
 
-	-- Spellbook side tabs
+	---------------------------------------------------------------------
+	-- Spellbook
+	---------------------------------------------------------------------
+
 	for i = 1, 5 do
 		AddBorder(_G["SpellBookSkillLineTab"..i])
 	end
 
-	-- Spellbook/companion buttons
 	local function Button_OnDisable(self)
 		self:SetAlpha(0)
 	end
@@ -335,56 +377,155 @@ tinsert(applyFuncs, function()
 		self:SetAlpha(1)
 	end
 
-	-- Spellbook
 	for i = 1, SPELLS_PER_PAGE do
 		local button = _G["SpellButton" .. i]
 		AddBorder(button)
-		button:HookScript("OnDisable", Button_OnDisable)
-		button:HookScript("OnEnable", Button_OnEnable)
-		button.SpellName:SetFont("Interface\\AddOns\\PhanxMedia\\font\\DejaWeb-Bold.ttf", 16)
+		button.SpellName:SetFont(config.font, 16)
 		button.EmptySlot:SetTexture("")
 		button.UnlearnedFrame:SetTexture("")
-		_G["SpellButton" .. i .. "SlotFrame"]:SetTexture("")
+		_G["SpellButton" .. i .. "SlotFrame"]:SetTexture("") -- swirly thing
 		_G["SpellButton" .. i .. "IconTexture"]:SetTexCoord(0.06, 0.94, 0.06, 0.94)
+		button:HookScript("OnDisable", Button_OnDisable)
+		button:HookScript("OnEnable", Button_OnEnable)
 	end
 
-	-- Spellbook / Core Abilities
 	hooksecurefunc("SpellBook_UpdateCoreAbilitiesTab", function()
-		for i, button in ipairs(SpellBookCoreAbilitiesFrame.Abilities) do
-			button.FutureTexture:SetTexture("")
-			button.Name:SetFont("Interface\\AddOns\\PhanxMedia\\font\\DejaWeb-Bold.ttf", 16)
+		for i = 1, #SpellBookCoreAbilitiesFrame.Abilities do
+			local button = SpellBookCoreAbilitiesFrame.Abilities[i]
+			if not button.BorderTextures then
+				AddBorder(button)
+				button.iconTexture:SetTexCoord(0.05, 0.95, 0.05, 0.95)
+				button.Name:SetFont(config.font, 16)
+				button.FutureTexture:SetTexture("")
+				select(3, button:GetRegions()):SetTexture("") -- swirly thing
+				local a, b, c, x, y = button.Name:GetPoint(1)
+				button.Name:SetPoint(a, b, c, x, 3)
+			end
+		end
+	end)
+
+	---------------------------------------------------------------------
+	-- Done!
+	---------------------------------------------------------------------
+
+	return true
+end)
+
+------------------------------------------------------------------------
+--	Blizzard_DebugTools
+------------------------------------------------------------------------
+
+tinsert(applyFuncs, function()
+	if EventTraceTooltip then
+		AddBorder(EventTraceTooltip)
+		AddBorder(FrameStackTooltip)
+		return true
+	end
+end)
+
+------------------------------------------------------------------------
+--	Blizzard_GlyphUI
+------------------------------------------------------------------------
+
+tinsert(applyFuncs, function()
+	if not GlyphFrame_UpdateGlyphList then return end
+
+	hooksecurefunc("GlyphFrame_UpdateGlyphList", function()
+		local buttons = GlyphFrame.scrollFrame.buttons
+		for i = 1, #buttons do
+			local button = buttons[i]
 			AddBorder(button)
+			button:SetBorderSize(nil, 3, -118, 3, 3)
+			button.icon:SetDrawLayer("ARTWORK")
 		end
 	end)
 
 	return true
 end)
 
+------------------------------------------------------------------------
+--	Blizzard_PetBattleUI
+------------------------------------------------------------------------
+
 tinsert(applyFuncs, function()
-	if not PlayerTalentFrame then return end
-	for row = 1, 6 do
-		for col = 1, 3 do
-			local button = _G["PlayerTalentFrameTalentsTalentRow"..row.."Talent"..col]
-			local frame = CreateFrame("Frame", nil, button)
-			frame:SetAllPoints(button.icon)
-			AddBorder(frame, nil, 4)
+	if not PetBattleFrame then return end
+
+	hooksecurefunc("PetBattleFrame_UpdateAllActionButtons", function(self)
+		--print("PetBattleFrame_UpdateAllActionButtons")
+		local f = self.BottomFrame
+		if f.CatchButton.BorderTextures then return end
+
+		AddBorder(f.CatchButton, nil, 4)
+		AddBorder(f.ForfeitButton, nil, 4)
+		AddBorder(f.SwitchPetButton, nil, 4)
+
+		for i = 1, #f.abilityButtons do
+			AddBorder(f.abilityButtons[i], nil, 4)
 		end
-	end
+	end)
+
+	-- Fix battle pet ability selection glow not appearing after the first turn
+	hooksecurefunc("PetBattleActionButton_UpdateState", function(self)
+		if not self.SelectedHighlight then return end
+		local actionType, actionIndex = self.actionType, self.actionIndex
+		local selectedType, selectedIndex = C_PetBattles.GetSelectedAction()
+		self.SelectedHighlight:SetShown(selectedType and selectedType == actionType and (not actionIndex or selectedIndex == actionIndex))
+	end)
+
 	return true
 end)
+
+------------------------------------------------------------------------
+--	Blizzard_PetJournal
+------------------------------------------------------------------------
 
 tinsert(applyFuncs, function()
 	if not MountJournalListScrollFrame then return end
 
+	local function qualityBorder_SetVertexColor(self, r, g, b)
+		self:GetParent().dragButton:SetBorderColor(r, g, b)
+	end
+
+	---------------------------------------------------------------------
 	-- Mount Journal
+	---------------------------------------------------------------------
+
+	local function FixTexture(icon, texture)
+		if texture == "Interface\\PetBattles\\MountJournalEmptyIcon" then
+			icon:SetTexCoord(0.05, 0.95, 0.05, 0.95)
+			icon:GetParent():Hide()
+		else
+			icon:SetTexCoord(0, 1, 0, 1)
+			icon:GetParent():Show()
+		end
+	end
 	for i = 1, #MountJournalListScrollFrame.buttons do
 		local button = MountJournalListScrollFrame.buttons[i]
 		AddBorder(button.DragButton, nil, 4)
+		hooksecurefunc(button.icon, "SetTexture", FixTexture)
 	end
 
+	---------------------------------------------------------------------
 	-- Pet Journal
+	---------------------------------------------------------------------
+
 	AddBorder(PetJournalHealPetButton)
 	PetJournalHealPetButtonBorder:SetTexture("")
+
+	do
+		local f = PetJournalPetCardPetInfo
+
+		local iconFrame = CreateFrame("Frame", nil, f)
+		iconFrame:SetAllPoints(f.icon)
+		AddBorder(iconFrame, nil, 4)
+
+		f.favorite:SetParent(iconFrame)
+		f.levelBG:SetParent(iconFrame)
+		f.level:SetParent(iconFrame)
+
+		f.dragButton = iconFrame
+		hooksecurefunc(f.qualityBorder, "SetVertexColor", qualityBorder_SetVertexColor)
+	end
 
 	for i = 1, 6 do
 		AddBorder(_G["PetJournalPetCardSpell"..i], nil, 4)
@@ -393,10 +534,6 @@ tinsert(applyFuncs, function()
 	for i = 1, 2 do
 		AddBorder(_G["PetJournalSpellSelectSpell"..i], nil, 4)
 		select(i, PetJournalSpellSelect:GetRegions()):SetTexture("")
-	end
-
-	local function qualityBorder_SetVertexColor(self, r, g, b)
-		self:GetParent().dragButton:SetBorderColor(r, g, b)
 	end
 
 	for i = 1, 3 do
@@ -411,105 +548,44 @@ tinsert(applyFuncs, function()
 		end
 	end
 
-	local f = PetJournalPetCardPetInfo
-	local iconFrame = CreateFrame("Frame", nil, f)
-	iconFrame:SetAllPoints(f.icon)
-	AddBorder(iconFrame, nil, 4)
-	f.favorite:SetParent(iconFrame)
-	f.levelBG:SetParent(iconFrame)
-	f.level:SetParent(iconFrame)
-	f.dragButton = iconFrame
-	hooksecurefunc(f.qualityBorder, "SetVertexColor", qualityBorder_SetVertexColor)
+	local function IconBorder_SetVertexColor(iconBorder, ...)
+		return iconBorder:GetParent().dragButton:SetBorderColor(...)
+	end
+
+	for i = 1, #PetJournalListScrollFrame.buttons do
+		local button = PetJournalListScrollFrame.buttons[i]
+		AddBorder(button.dragButton, nil, 4)
+		button.icon:SetTexCoord(0.06, 0.94, 0.06, 0.94)
+		button.iconBorder.SetVertexColor = IconBorder_SetVertexColor
+		button.iconBorder.Hide = IconBorder_SetVertexColor
+	end
 
 	return true
 end)
 
-tinsert(applyFuncs, function()
-	if EventTraceTooltip then
-		AddBorder(EventTraceTooltip)
-		return true
-	end
-end)
+------------------------------------------------------------------------
+--	Blizzard_TalentUI
+------------------------------------------------------------------------
 
 tinsert(applyFuncs, function()
-	if FrameStackTooltip then
-		AddBorder(FrameStackTooltip)
-		return true
-	end
-end)
+	if not PlayerTalentFrame then return end
 
---[[
-tinsert(applyFuncs, function()
-		PetBattleFrame:HookScript("OnShow", function()
-			AddBorder(PetBattleFrame.BottomFrame.abilityButtons[1], nil, 4)
-			AddBorder(PetBattleFrame.BottomFrame.abilityButtons[2], nil, 4)
-			AddBorder(PetBattleFrame.BottomFrame.abilityButtons[3], nil, 4)
-			AddBorder(PetBattleFrame.BottomFrame.SwitchPetButton, nil, 4)
-			AddBorder(PetBattleFrame.BottomFrame.CatchButton, nil, 4)
-			AddBorder(PetBattleFrame.BottomFrame.ForfeitButton, nil, 4)
-		end)
+	AddBorder(PlayerTalentFrameSpecializationSpellScrollFrameScrollChildAbility1, nil, -4)
+	PlayerTalentFrameSpecializationSpellScrollFrameScrollChildAbility1.ring:Hide()
 
-		local function AddButtonBorder(button)
-			local f = CreateFrame("Frame", nil, button)
-			f:SetAllPoints(button.icon)
-			button.iconBorderFrame = f
+	hooksecurefunc("PlayerTalentFrame_CreateSpecSpellButton", function(self, index)
+		local f = self.spellsScroll.child["abilityButton"..index]
+		AddBorder(f, nil, -4)
+		f.ring:Hide()
+	end)
 
-			AddBorder(f, nil, 3)
-
-			button.icon:SetTexCoord(0.06, 0.94, 0.06, 0.94)
-
-			if button.iconBorder then
-				function button.iconBorder:SetVertexColor(...) return f:SetBorderColor(...) end
-				function button.iconBorder:Hide() return f:SetBorderColor() end
-			end
-
-			if button.dragButton then
-				button.dragButton.levelBG:SetParent(f)
-				button.dragButton.level:SetParent(f)
-			end
+	for row = 1, 6 do
+		for col = 1, 3 do
+			local button = _G["PlayerTalentFrameTalentsTalentRow"..row.."Talent"..col]
+			AddBorder(button)
+			button:SetBorderSize(nil, -30, -110, 0, 0)
 		end
-
-		local function UpdateButtonBorders(scrollFrame)
-			local offset = HybridScrollFrame_GetOffset(scrollFrame)
-			for i, button in ipairs(scrollFrame.buttons) do
-				if not button.BorderTextures then
-					AddButtonBorder(button)
-				end
-				if button.dragButton then
-					local petID, rarity, _ = button.petID
-					if petID then
-						_, _, _, _, rarity = C_PetJournal.GetPetStats(petID)
-					end
-					if rarity then
-						local color = ITEM_QUALITY_COLORS[rarity - 1]
-						button.name:SetTextColor(color.r, color.g, color.b)
-						button.iconBorderFrame:SetBorderColor(color.r, color.g, color.b)
-					else
-						local color = ITEM_QUALITY_COLORS[0]
-						button.name:SetTextColor(color.r, color.g, color.b)
-						button.iconBorderFrame:SetBorderColor(color.r, color.g, color.b)
-					end
-				end
-			end
-		end
-
-		PetJournalListScrollFrame:HookScript("OnShow", UpdateButtonBorders)
-		PetJournalListScrollFrame:HookScript("OnMouseWheel", UpdateButtonBorders)
-		PetJournalListScrollFrameScrollBarScrollDownButton:HookScript("OnClick", function(self)
-			UpdateButtonBorders(PetJournalListScrollFrame)
-		end)
-		PetJournalListScrollFrameScrollBarScrollUpButton:HookScript("OnClick", function(self)
-			UpdateButtonBorders(PetJournalListScrollFrame)
-		end)
-		hooksecurefunc("PetJournal_UpdatePetList", function()
-			UpdateButtonBorders(PetJournalListScrollFrame)
-		end)
-
-		hooksecurefunc("MountJournal_UpdateMountList", function()
-			UpdateButtonBorders(MountJournalListScrollFrame)
-		end)
-
-		return true
 	end
+
+	return true
 end)
-]]
