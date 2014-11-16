bd90a15e02bae4d47aad6655fb98c1c62e47a0c1
Phanx <addons@phanx.net>
2014-11-16 01:58:12 -0800

    Post SVN->Git cleanup

--- Addons.lua
+++ Addons.lua
@@ -1,15 +1,8 @@
 --[[--------------------------------------------------------------------
 	PhanxBorder
-	World of Warcraft user interface addon:
 	Adds shiny borders to things.
 	Copyright (c) 2008-2014 Phanx <addons@phanx.net>. All rights reserved.
-
-	Please DO NOT upload this addon to other websites, or post modified
-	versions of it. However, you are welcome to include a copy of it
-	WITHOUT CHANGES in compilations posted on Curse and/or WoWInterface.
-	You are also welcome to use any/all of its code in your own addon, as
-	long as you do not use my name or the name of this addon ANYWHERE in
-	your addon, including its name, outside of an optional attribution.
+	https://github.com/Phanx/PhanxBorder
 ----------------------------------------------------------------------]]
 
 local USE_CLASS_COLOR = true
